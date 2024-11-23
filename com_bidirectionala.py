f50196e3976903b14ef98a8976934acc3cc26b97
anacalapis <128613566+anacalapis@users.noreply.github.com>
2024-11-23 22:18:33 +0200

    test comunicare bidirectionala esp-Raspberry

---
+++ com_bidirectionala.py
@@ -0,0 +1,48 @@
+from bluetooth import *
+
+def input_and_send():
+	print("\n Raspberry -> ESP32\n")
+	while True:
+		data = input()
+		if len(data) == 0: break
+		sock.send(data)
+		sock.send("\n")
+	
+def rx_and_echo():
+	sock.send("\nESP -> Raspberry")
+	while True:
+		data = sock.recv(buf_size)
+		if data:
+			print(data)
+			sock.send(data)
+			
+addr= "A0:A3:B3:97:55:46"
+service_matches = find_service( address = addr )
+
+buf_size = 1024;
+
+
+if len(service_matches) == 0:
+    print("couldn't find the SampleServer service =(")
+    sys.exit(0)
+    
+#for s in range (len(service_matches)):
+#	print("\nservice_matches[" + str(s) + "]:")
+#	print(service_matches[s])
+	
+first_match = service_matches[0]
+port = first_match["port"]
+name = first_match["name"]
+host = first_match["host"]
+
+#print("connecting to \"%s\" on %s" % (name, host))
+
+# Create the client socket
+sock=BluetoothSocket( RFCOMM )
+sock.connect((host, port))
+
+#input_and_send()
+rx_and_echo()
+
+sock.close()
+
