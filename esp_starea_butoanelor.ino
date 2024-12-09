a88a656dfa313443aceb810eba34e46d7b22101a
anacalapis <128613566+anacalapis@users.noreply.github.com>
2024-12-09 18:56:44 +0200

    ESP- timoul se opreste din microfon

--- esp_starea_butoanelor.ino
+++ esp_starea_butoanelor.ino
@@ -2,11 +2,13 @@
 
 BluetoothSerial SerialBT; // Obiect pentru comunicare Bluetooth
 
-#define PAUSE_BUTTON 23 // Pinul la care este conectat butonul
+#define MIC_PIN 23 // Pinul la care este conectat microfonul
 #define RESUME_BUTTON 22
 
+
+int flag=0;
 void setup() {
-  pinMode(PAUSE_BUTTON, INPUT_PULLUP); // Configurare buton cu pull-up intern
+  pinMode(MIC_PIN, INPUT); // Configurare buton cu pull-up intern
   pinMode(RESUME_BUTTON, INPUT_PULLUP);
   Serial.begin(9600); // Inițializare Serial Monitor
   SerialBT.begin("ESP32_Button_Sender"); // Numele dispozitivului Bluetooth
@@ -14,15 +16,17 @@ void setup() {
 
 void loop() {
   // Citește starea butonului
-  int pause_button = digitalRead(PAUSE_BUTTON);
+  int microphone = digitalRead(MIC_PIN);
   int resume_button = digitalRead(RESUME_BUTTON);
 
   // Trimite starea butonului la Raspberry Pi prin Bluetooth
-  if (pause_button == LOW) {
+  if (microphone == HIGH && flag==1) {
     SerialBT.print(1);
+    flag=0;
   } 
   if (resume_button == LOW) {
     SerialBT.print(0);
+    flag=1;
   } 
 
   delay(200);
