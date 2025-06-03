4b03d45c0caed8264ccf6e5b88eeec325d360c88
Ana Calapis <anacalapis02@gmail.com>
2025-06-03 19:53:29 +0300

    noua abordare pe IMU

--- imu/imu.ino
+++ imu/imu.ino
@@ -6,69 +6,54 @@
 BluetoothSerial SerialBT;
 Adafruit_MPU6050 imuu;
 
-#define BUFFER_SIZE 30
-float buffer1[BUFFER_SIZE]={0}, buffer2[BUFFER_SIZE]={0};
-int cnt=0; //nr de elem din primul buffer
+#define CAPACITATE_VECTOR 30
+float vector_1[CAPACITATE_VECTOR]={0}, vector_2[CAPACITATE_VECTOR]={0};
+int nr_elemente=0; //nr de elem din primul buffer
 int i=0, j=0;
-float  max_buff1=0, max_buff2=0, maxim=0;
-int start=0, finalA=0;
-float maxim_anterior =0; 
+float  maxim_vector1=0, maxim_vector2=0, maxim=0;
+bool start_prelucrare=false, final_vector1=false;
+float maxim_prelucrare_anterioara =0; 
 
-void setup() {
-  Serial.begin(9600); 
-  // imuu.begin();
+void setup() 
+{
   SerialBT.begin("ESP_IMU"); 
-
   Wire.setClock(400000);
   Wire.begin();
   delay(250);
-  // Wire.beginTransmission(0x68); //începe o transmisie către dispozitivul I2C cu adresa 0x68,
-  // Wire.write(0x6B);             //gestionarea alimentării senzorului
-  // Wire.write(0x00);             //senzorul va ieși din modul de repaus și va începe să fie activ
-  // Wire.endTransmission();
   if (!imuu.begin()) {
   Serial.println("Eroare IMU!");
   while (1);
 }
-
-  // imuu.setAccelerometerRange(MPU6050_RANGE_8_G);
-  // imuu.setGyroRange(MPU6050_RANGE_500_DEG);
-  // imuu.setFilterBandwidth(MPU6050_BAND_21_HZ);
-  //Wire.begin();
 }
 
 void loop()
 {
-  
-  if(start==0)
+  if(start_prelucrare == false)
   {
-    prelucrare_initiala(cnt);
-    start=1;
+    prelucrare_initiala(nr_elemente);
+    start_prelucrare=true;
   }
-  float fct = final_vector();
-  if(maxim_anterior <= fct)
+  float rezultat = final_vector();
+  if(maxim_prelucrare_anterioara <= rezultat)
   {
-    maxim_anterior=fct;
+    maxim_prelucrare_anterioara=rezultat;
   }
   else
   {
-    if((int)maxim_anterior != (int)fct)
+    if((int)maxim_prelucrare_anterioara != (int)rezultat)
     {
-      int masuratoare= (int)maxim_anterior;
-      Serial.println(masuratoare);
+      int masuratoare= (int)maxim_prelucrare_anterioara;
       SerialBT.println(masuratoare);
-      maxim_anterior=0;
+      maxim_prelucrare_anterioara=0;
     }
-    
   }
-  //SerialBT.println("1");
   delay(85);
 }
 
 
-int prelucrare_initiala(int cnt)
+int prelucrare_initiala(int nr_elemente)
 {
-  while(cnt<40)
+  while(nr_elemente<40)
   {
     sensors_event_t accel;
     imuu.getAccelerometerSensor()->getEvent(&accel);
@@ -77,71 +62,80 @@ int prelucrare_initiala(int cnt)
         accel.acceleration.y * accel.acceleration.y +
         accel.acceleration.z * accel.acceleration.z
     );
-    if(cnt<10)
+    if(nr_elemente<10)
     {
-      buffer1[i++]= acceleratie;
+      vector_1[i++]= acceleratie;
     }
     else
     {
-      if(cnt<30)
+      if(nr_elemente<30)
       {
-        buffer1[i++]=acceleratie;
-        buffer2[j++]=acceleratie;
+        vector_1[i++]=acceleratie;
+        vector_2[j++]=acceleratie;
       }
-      if(cnt>=30 && cnt<40)
+      if(nr_elemente>=30 && nr_elemente<40)
       {
-        buffer2[j++]=acceleratie;
+        vector_2[j++]=acceleratie;
       }
     }
-    cnt++;
+    nr_elemente++;
 
   }
-  return cnt;
-  
+  return nr_elemente; 
 }
 
 float final_vector()
 {
-  float max_buff1=maxim_vector(buffer1);
-  float max_buff2=maxim_vector(buffer2);
+  float maxim_vector1=maxim_vector(vector_1);
+  float maxim_vector2=maxim_vector(vector_2);
 
-  if(finalA==0) //cand e gata buffer B, adica (cnt-30)%20 ==0
+  if(final_vector1==false) //cand e gata buffer B, adica (nr_elemente-30)%20 ==0
   {
-    for(int i=0; i<20; i++)
+    for(int i=0; i<10; i++)
     {
-      buffer1[i]=buffer1[i+10];
+      vector_1[i]=vector_1[i+20];
+      vector_1[i+10]= vector_2[20+i];
     }
-    finalA=1;
-  }
-  else
-  {
-    for(int i=0; i<20; i++)
+    for(int i=20; i< CAPACITATE_VECTOR; i++)
     {
-      buffer2[i]=buffer2[i+10];
-    }
-    finalA=0;
-  }
-  for(int j=20; j<BUFFER_SIZE; j++)
-  {
-    sensors_event_t accel;
-    imuu.getAccelerometerSensor()->getEvent(&accel);
-    float acceleratie = sqrt(
+      sensors_event_t accel;
+      imuu.getAccelerometerSensor()->getEvent(&accel);
+      float acceleratie = sqrt(
         accel.acceleration.x * accel.acceleration.x +
         accel.acceleration.y * accel.acceleration.y +
         accel.acceleration.z * accel.acceleration.z
-    );
-    buffer1[j]=acceleratie;
-    buffer2[j]=acceleratie;
+      );
+      vector_1[i]=acceleratie;
+    }
+    final_vector1=true;
   }
-  cnt=cnt+10; //pt ca s-au citit alte 10 elem noi
-
-  return max(max_buff1, max_buff2);
+  else
+  {
+    for(int i=0; i<10; i++)
+    {
+      vector_2[i]=vector_2[i+20];
+      vector_2[i+10]= vector_1[20+i];
+    }
+    for(int i=20; i< CAPACITATE_VECTOR; i++)
+    {
+      sensors_event_t accel;
+      imuu.getAccelerometerSensor()->getEvent(&accel);
+      float acceleratie = sqrt(
+        accel.acceleration.x * accel.acceleration.x +
+        accel.acceleration.y * accel.acceleration.y +
+        accel.acceleration.z * accel.acceleration.z
+      );
+      vector_2[i]=acceleratie;
+    }
+    final_vector1=false;
+  }
+  return max(maxim_vector1, maxim_vector2);
 }
 
 float maxim_vector(float vector[])
 {
   float maxim=vector[0];
-  for(int i=1; i<=BUFFER_SIZE; i++)
+  for(int i=1; i<=CAPACITATE_VECTOR; i++)
   {
     if(vector[i]>maxim)
     {
